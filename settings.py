32f4a191b6a760878008e5b45e7f2c3bca4344a2
Vladyslav <vladyslav.teron@gmail.com>
2023-03-20 20:43:19 +0200

    Added: New alphabets

--- settings.py
+++ settings.py
@@ -3,6 +3,10 @@ WINDOW_MIN_HEIGHT = 400
 BACKGROUND_COLOR = '#282A3A'
 FONT_COLOR = 'white'
 DEFAULT_FONT = ('Helvetica', 11)
-ALPHABETS = [("ABCDEFGHIJKLMNOPQRSTUVWXYZ .,_", "English"),         # English alphabet
-             (".АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ_,!", "Ukrainian") # Ukrainian alphabet
+ALPHABETS = [("ABCDEFGHIJKLMNOPQRSTUVWXYZ .,_", "English"),  # English alphabet
+             ("ABCDEFGHIJKLMNOPQRSTUVWXYZ .,_=&!*%@", "English (Perfect Square)"),  # English alphabet (Number of alphabetic characters is a Perfect Square),
+             ("ABCDEFGHIJKLMNOPQRSTUVWXYZ ", "English (27 characters)"),  # English alphabet with 27 characters
+             (".АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ_,!", "Ukrainian "),  # Ukrainian alphabet
+             ("АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ ,!", "Ukrainian (Perfect Square)"),  # Ukrainian alphabet (Number of alphabetic characters is a Perfect Square)
+             ("АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ .,-", "Ukrainian (37 characters)")  # Ukrainian alphabet with 37 charaters
              ]
