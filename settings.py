181c0d8d2aa3f1cac74b179a11e755ac207ee7b2
Vladyslav <vladyslav.teron@gmail.com>
2023-03-24 12:42:01 +0200

    Fixed alphabet

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ DEFAULT_FONT = ('Helvetica', 11)
 ALPHABETS = [("ABCDEFGHIJKLMNOPQRSTUVWXYZ .,_", "English"),  # English alphabet
              ("ABCDEFGHIJKLMNOPQRSTUVWXYZ .,_=&!*%@", "English (Perfect Square)"),  # English alphabet (Number of alphabetic characters is a Perfect Square),
              ("ABCDEFGHIJKLMNOPQRSTUVWXYZ ", "English (27 characters)"),  # English alphabet with 27 characters
-             (".АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ_,!", "Ukrainian "),  # Ukrainian alphabet
+             ("АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ _,!.", "Ukrainian "),  # Ukrainian alphabet
              ("АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ ,!", "Ukrainian (Perfect Square)"),  # Ukrainian alphabet (Number of alphabetic characters is a Perfect Square)
              ("АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ .,-", "Ukrainian (37 characters)")  # Ukrainian alphabet with 37 charaters
              ]
